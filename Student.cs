6ba247d3d2ce8fa2184498442138ef11862d5fc8
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2024-09-16 06:51:39 +0700

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -8,9 +8,24 @@ namespace BTTuan2
 {
     internal class Student
     {
-        public int Id { get; set; }
-        public string Name { get; set; } 
-        public int Age { get; set; } 
+        private int id;
+        private string name;
+        private int age;
+
+        public Student()
+        {
+        }
+
+        public Student(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
 
         public void Input()
         {
