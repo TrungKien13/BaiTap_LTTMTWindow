89bc43c9e6f60f6dca175c5b23384cefc9d18a20
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2024-09-16 06:45:17 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,54 +10,85 @@ namespace BTTuan2
     {
         static void Main(string[] args)
         {
-            List<Student> DSHS = new List<Student>
-            {
-                new Student { Id = 1, Name = "An", Age = 17 },
-                new Student { Id = 2, Name = "Bình", Age = 16 },
-                new Student { Id = 3, Name = "Anh", Age = 18 },
-                new Student { Id = 4, Name = "Dương", Age = 15 },
-                new Student { Id = 5, Name = "Cường", Age = 20 }
-            };
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Write("Nhập số lượng học sinh: ");
+            int soLuongHocSinh = int.Parse(Console.ReadLine());
 
-            // a. In danh sách toàn bộ học sinh
-            Console.WriteLine("Danh sách toàn bộ học sinh:");
-            foreach (var hs in DSHS)
+            List<Student> danhSachHS = new List<Student>();
+
+            for (int i = 0; i < soLuongHocSinh; i++)
             {
-                Console.WriteLine($"ID: {hs.Id}, Tên: {hs.Name}, Tuổi: {hs.Age}");
+                Console.WriteLine($"\nNhập thông tin cho học sinh thứ {i + 1}:");
+                Student hs = new Student();
+                hs.Input(); 
+                danhSachHS.Add(hs);
             }
 
-            // b. Tìm và in ra danh sách các học sinh có tuổi từ 15 đến 18
-            var HSAge15den18 = DSHS.Where(hs => hs.Age >= 15 && hs.Age <= 18);
-            Console.WriteLine("\nDanh sách học sinh có tuổi từ 15 đến 18:");
-            foreach (var hs in HSAge15den18)
+            // In danh sách học sinh
+            Console.WriteLine("\nDanh sách toàn bộ học sinh:");
+            foreach (var hs in danhSachHS)
             {
-                Console.WriteLine($"ID: {hs.Id}, Tên: {hs.Name}, Tuổi: {hs.Age}");
+                hs.Show();
             }
 
-            // c. Tìm và in ra học sinh có tên bắt đầu bằng chữ "A"
-            var HSNameA = DSHS.Where(hs => hs.Name.StartsWith("A"));
-            Console.WriteLine("\nDanh sách học sinh có tên bắt đầu bằng chữ 'A':");
-            foreach (var hs in HSNameA)
-            {
-                Console.WriteLine($"ID: {hs.Id}, Tên: {hs.Name}, Tuổi: {hs.Age}");
-            }
+            TimHSTheoTuoi(danhSachHS, 15, 18);
 
-            // d. Tính tổng tuổi của tất cả học sinh trong danh sách
-            var tongTuoi = DSHS.Sum(hs => hs.Age);
+            TimHSTheoTen(danhSachHS, "A");
+
+            TinhTongTuoi(danhSachHS);
+
+            TimHSTuoiLonNhat(danhSachHS);
+
+            SapXepHSTheoTuoi(danhSachHS);
+        }
+
+        // Tìm học sinh theo độ tuổi
+        static void TimHSTheoTuoi(List<Student> ds, int tuoiMin, int tuoiMax)
+        {
+            var hocSinhTuoiThichHop = ds.Where(hs => hs.Age >= tuoiMin && hs.Age <= tuoiMax);
+            Console.WriteLine($"\nHọc sinh có tuổi từ {tuoiMin} đến {tuoiMax}:");
+            foreach (var hs in hocSinhTuoiThichHop)
+            {
+                hs.Show();
+            }
+        }
+
+        // Tìm học sinh có tên bắt đầu bằng ký tự
+        static void TimHSTheoTen(List<Student> ds, string kyTu)
+        {
+            var hocSinhTenBatDauBang = ds.Where(hs => hs.Name.StartsWith(kyTu));
+            Console.WriteLine($"\nHọc sinh có tên bắt đầu bằng chữ '{kyTu}':");
+            foreach (var hs in hocSinhTenBatDauBang)
+            {
+                hs.Show();
+            }
+        }
+
+        // Tính tổng tuổi học sinh
+        static void TinhTongTuoi(List<Student> ds)
+        {
+            var tongTuoi = ds.Sum(hs => hs.Age);
             Console.WriteLine($"\nTổng tuổi của tất cả học sinh: {tongTuoi}");
+        }
 
-            // e. Tìm và in ra học sinh có tuổi lớn nhất
-            var HSAgeMax = DSHS.OrderByDescending(hs => hs.Age).First();
-            Console.WriteLine($"\nHọc sinh có tuổi lớn nhất: ID: {HSAgeMax.Id}, Tên: {HSAgeMax.Name}, Tuổi: {HSAgeMax.Age}");
+        // Tìm học sinh có tuổi lớn nhất
+        static void TimHSTuoiLonNhat(List<Student> ds)
+        {
+            var hocSinhTuoiLonNhat = ds.OrderByDescending(hs => hs.Age).FirstOrDefault();
+            Console.WriteLine($"\nHọc sinh có tuổi lớn nhất:");
+            hocSinhTuoiLonNhat.Show();
+        }
 
-            // f. Sắp xếp danh sách học sinh theo tuổi tăng dần và in ra danh sách sau khi sắp xếp
-            var danhSachSapXep = DSHS.OrderBy(hs => hs.Age);
-            Console.WriteLine("\nDanh sách học sinh sau khi sắp xếp theo tuổi tăng dần:");
-            foreach (var hs in danhSachSapXep)
+        // Sắp xếp học sinh theo tuổi tăng dần
+        static void SapXepHSTheoTuoi(List<Student> ds)
+        {
+            var dsSapXepTheoTuoi = ds.OrderBy(hs => hs.Age);
+            Console.WriteLine("\nDanh sách học sinh theo tuổi tăng dần:");
+            foreach (var hs in dsSapXepTheoTuoi)
             {
-                Console.WriteLine($"ID: {hs.Id}, Tên: {hs.Name}, Tuổi: {hs.Age}");
+                hs.Show();
             }
-            Console.ReadKey();  
         }
     }
 }
