489b46ca16cb1fa9a1c2550398216f5df0e97195
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2024-09-16 06:46:44 +0700

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +9,24 @@ namespace BTTuan2
     internal class Student
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public string Name { get; set; } 
+        public int Age { get; set; } 
+
+        public void Input()
+        {
+            Console.Write("Nhập mã số: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Nhập tên: ");
+            Name = Console.ReadLine();
+
+            Console.Write("Nhập tuổi: ");
+            Age = int.Parse(Console.ReadLine());
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Mã số: {this.Id}, Tên: {this.Name}, Tuổi: {this.Age}");
+        }
     }
 }
+
